6b0f540a9ee3c4bbbc53b5c43aee92db31fd1499
Parashar Regmi <parasharregmi@gmail.com>
2026-07-17 16:53:26 +0545

    Final: hardcode _method in URL, remove hidden input

--- resources/views/admin/registrations/edit.blade.php
+++ resources/views/admin/registrations/edit.blade.php
@@ -38,12 +38,11 @@
 {{-- ===== MAIN CARD ===== --}}
 <div style="background:#fff; border-radius:16px; padding:30px; box-shadow:0 2px 12px rgba(0,0,0,0.04); border:1px solid #e2e8f0;">
 
-<form action="{{ route('admin.registrations.update', $registration) }}" 
+<form action="{{ route('admin.registrations.update', $registration) }}?_method=PUT" 
       method="POST" 
       enctype="multipart/form-data" 
       id="editForm">
               @csrf
-        <input type="hidden" name="_method" value="PUT">
 
         {{-- ============================================================ --}}
         {{-- SECTION 1: HOSTEL INFORMATION --}}
